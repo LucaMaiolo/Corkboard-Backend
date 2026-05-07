76678caee98b9e87d33b7cc193bc7575e41a2fe7
Luca Maiolo <lucamaiolo07@gmail.com>
2026-05-07 16:21:02 -0400

    launch.json fixed

---
+++ history/lucamaiolo/.history/Corkboard-Backend/src/controllers/homeController_20260507155002.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import type { Request, Response } from "express";
+import { authenticateUser, refreshSession } from "./sessionController.js";
+const router = express.Router();
+const routeRoot = "/";
+router.get("/", showHome);
+function showHome(request: Request, response: Response): void {
+  const authenticatedUser = authenticateUser(request);
+    if (!authenticatedUser) {
+      refreshSession(request, response);
+      response.sendStatus(401); 
+    return;
+}
+console.log(`User ${authenticatedUser.userSession.username} is authorized for home page`);
+
+  response.status(200);
+  response.status(200).json({ message: "Welcome!", username: authenticatedUser.userSession.username });
+
+}
+export { router, routeRoot };
